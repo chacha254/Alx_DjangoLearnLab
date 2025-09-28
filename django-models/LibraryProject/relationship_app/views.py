ff88490ebde0ee07251c815018b9f76153d5da88
sir charles onyango <charlesotieno404@gmail.com>
2025-09-28 18:20:03 +0300

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
