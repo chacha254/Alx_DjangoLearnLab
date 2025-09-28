b9b98b360b7081037e8b8a7d30ebf191a31aa66b
sir charles onyango <charlesotieno404@gmail.com>
2025-09-28 17:56:30 +0300

    .

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .views import book_list
+from .views import list_books
 
 urlpatterns = [
-    path('books/', views.book_list, name="book_list"),
+    path('books/', views.list_books, name="book_list"),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name="library_detail"),
 ]
