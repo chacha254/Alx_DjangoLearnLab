2cf633f91408d89e93a7a03a35e262812585095f
sir charles onyango <charlesotieno404@gmail.com>
2025-09-28 18:24:45 +0300

    .

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import register, CustomLoginView, CustomLogoutView, list_books
+from .views import register, LoginView, LogoutView, list_books
 
 urlpatterns = [
     path('books/', views.list_books, name="book_list"),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
-    path("register/", register, name="register"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
+    path("register/", views.register, name="register"),
 ]
